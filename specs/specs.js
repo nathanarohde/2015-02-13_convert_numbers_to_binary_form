48cde1df081c6695261d5121b26e8b19f606899a
monty rohde <nathanarohde@gmail.com>
2015-02-13 10:58:38 -0800

    Tests every possible variable relation scenario to make sure proper result is returned.

--- specs/specs.js
+++ specs/specs.js
@@ -1,6 +1,12 @@
 describe('triangle_identifier', function() {
-  it('Program returns not a triangle when it is not a triangle.', function() {
+  it('When side_c > (side_a + side_b) program returns not a triangle when it is not a triangle.', function() {
     expect(triangleIdentifier(2, 2, 5)).to.equal('These sides can not make a triangle.');
+  });
+  it('When side_b > (side_c + side_a) program returns not a triangle when it is not a triangle.', function() {
+    expect(triangleIdentifier(2, 5, 2)).to.equal('These sides can not make a triangle.');
+  });
+  it('When side_a > (side_b + side_c) program returns not a triangle when it is not a triangle.', function() {
+    expect(triangleIdentifier(5, 2, 2)).to.equal('These sides can not make a triangle.');
   });
   it('Correctly identifies when a triangle is scalene.', function() {
     expect(triangleIdentifier(2, 3, 4)).to.equal('This triangle is scalene.');
